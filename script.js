f206aa93fa273d43b9bab5583a9e1db7e30f4d17
gianluca galota <gianluca.galota@gmail.com>
2021-07-08 17:57:56 +0100

    scroll smooth nav links

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 'use strict';
 
-// Modal window
 
+// Selecting DOM Elements
+const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnsNavLinks = document.querySelectorAll('.nav__link');
 
+// Modal window
 const openModal = (e) => {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -28,10 +33,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// Selecting DOM Elements
-const header = document.querySelector('.header');
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+
 
 // Creating DOM Elements
 const msgCookie = document.createElement('div');
@@ -40,6 +42,17 @@ msgCookie.classList.add('cookie-message');
 msgCookie.innerHTML = `<p>This website contains cookies for a better functionality</p> <button class="btn">Got it</button>`;
 header.append(msgCookie);
 
+// Smooth Scrolling
+btnScrollTo.addEventListener('click', () => section1.scrollIntoView({ behavior: 'smooth' }));
+// Smooth Scrolling on Nav Links
+btnsNavLinks.forEach(btn => btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const id = btn.getAttribute('href');
+  document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+}))
+
 // Events Handler
 msgCookie.addEventListener('click', () => msgCookie.remove(msgCookie));
-btnScrollTo.addEventListener('click', () => section1.scrollIntoView({ behavior: 'smooth' }));
+
+
+
